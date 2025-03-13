f26170acf926e65915848da54194467deda7e39c
Mr-Dukey <harimahe.1317@gmail.com>
2025-03-13 15:46:18 +0530

    cookie update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const {dbConnection} = require('./database/dbConnection');
-
+const cookieParser = require('cookie-parser');
 app.use(cors({
     origin: process.env.CORS,
     credentials: true,
@@ -13,6 +13,7 @@ app.use(cors({
 }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser())
 
 //DB connection
 dbConnection();
